3b752ed2e78c4bf117cabd2b323c41fc3578fb1b
CoderJay06 <jaycru06@yahoo.com>
2021-03-12 05:48:30 -0500

    commit tree 2efcb39b1cfcb0b7ba3b93501c4f1800f7c443d1

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,10 +1,7 @@
 export function managePresents(state = { numberOfPresents: 0 }, action){
    switch (action.type) {
       case 'INCREASE':
-         // const newState = Object.assign({}, state);
-         // newState.numberOfPresents++;
-         // return newState
-         return { numberOfPresents: state.numberOfPresents + 1 };
+         return Object.assign({}, state, { numberOfPresents: numberOfPresents + 1});
       default:
          return state;
    }
